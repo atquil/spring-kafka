b591049c218f3f5bb08258bd54937227b03a5047
aulanand <atulanandmishra@gamil.com>
2022-09-23 03:38:18 +0530

    Added changes to config

--- src/main/java/com/atquil/springkafka/controller/KafkaDemoController.java
+++ src/main/java/com/atquil/springkafka/controller/KafkaDemoController.java
@@ -1,10 +1,8 @@
 package com.atquil.springkafka.controller;
 
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.atquil.springkafka.entity.PopulationList;
