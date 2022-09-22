19c0d39b936dc2684fa7cf9a1e315ab5de0aa6ad
aulanand <atulanandmishra@gamil.com>
2022-09-22 15:26:48 +0530

    Added rest endpoint

---
+++ src/main/java/com/atquil/springkafka/controller/KafkaDemoController.java
@@ -0,0 +1,26 @@
+package com.atquil.springkafka.controller;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atquil.springkafka.entity.PopulationList;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class KafkaDemoController {
+    
+    private final KafkaTemplate<String , String> kafkaTemplate;
+
+    @PostMapping("/send")
+    public void kafkaController(@RequestBody PopulationList populationList){
+        System.out.print(populationList);
+        kafkaTemplate.send("atquil", populationList.toString());
+    }
+
+}
