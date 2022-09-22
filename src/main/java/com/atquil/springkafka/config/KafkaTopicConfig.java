29b069916a38fbd112cc3f73829fd0cd49001819
aulanand <atulanandmishra@gamil.com>
2022-09-23 03:59:59 +0530

    Adding changes for json

---
+++ src/main/java/com/atquil/springkafka/config/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package com.atquil.springkafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic createJsonTopic(){
+        return TopicBuilder.name("atquil_json").build();
+    }
+    
+}
